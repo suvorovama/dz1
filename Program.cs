f04fdd8fbdd6e51373eb2922d6e01d87cc94dee6
margarita <runlet@inbox.ru>
2022-08-07 22:33:25 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+
+Console.Write ("Введите число №1:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число №2:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber)
+{
+    Console.Write("Число №1 больше числа №2");
+}
+else
+{
+    Console.Write("Число №1 меньше числа №2");
+}
+
+Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.
+
+
+Console.Write("Введите число №1:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №3:");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if (secondNumber > max) max = secondNumber;
+if (thirdNumber > max) max = thirdNumber;
+Console.WriteLine(max);
+*/
